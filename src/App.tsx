0ce8a315868586a09a8f5661c41b21ef1ba18a42
Mohamad Hosein <amirmm556@gmail.com>
2024-09-03 02:43:55 +0330

    implement AddPost page and onCreatePost function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { useMemo } from "react"
 import { Route, Routes } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import { v4 as uuidV4 } from "uuid"
 
 import AddPost from "./components/AddPost"
+import { useLocaStorage } from "./hooks/useLocalStorage"
 
-export type Post = {
+export type RawPost = {
   id: string
-} & PostData
+} & RawPostData
+
+export type RawPostData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
 
 export type PostData = {
   title: string
@@ -20,11 +29,27 @@ export type Tag = {
 
 
 function App() {
+  const [posts, setPosts] = useLocaStorage<RawPost[]>({ key: "POSTS", initialValue: [] })
+  const [tags, setTags] = useLocaStorage<Tag[]>({ key: "TAGS", initialValue: [] })
+
+  const postsWithTags = useMemo(() => posts.map((item) => {
+    return {
+      ...item,
+      tags: tags.filter(tag => item.tagIds.includes(tag.id))
+    }
+  }), [posts, tags])
+
+  function onCreatePost({ tags, ...data }: PostData) {
+    setPosts((prevPosts) => {
+      return [...prevPosts, { ...data, id: uuidV4(), tagIds: tags.map(item => item.id) }]
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h2>Home Page</h2>} />
-        <Route path="/add" element={<AddPost />} />
+        <Route path="/add" element={<AddPost onSubmit={onCreatePost} />} />
         <Route path="/:id" >
           <Route index element={<h2>View Post</h2>} />
           <Route path="edit" element={<h2>Edit Post</h2>} />
--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,7 +1,15 @@
-function AddPost() {
+import { PostData } from "../App"
+import PostForm from "./PostForm"
+
+type AddPost = {
+    onSubmit: (data: PostData) => void
+}
+
+function AddPost({ onSubmit }: AddPost) {
     return (
         <>
             <h2>افزودن پست</h2>
+            <PostForm onSubmit={onSubmit} />
         </>
     )
 }
